1f78afb7c2ad76c5811b8d79a4562b923770d0bd
ChandraBahasuan <chandrabahasuan@gmail.com>
2025-07-10 04:03:13 +0700

    first commit

---
+++ ci4/app/Views/template/footer.php
@@ -0,0 +1,23 @@
+        </section> <!-- Tutup konten utama -->
+        <aside id="sidebar">
+            <div class="widget-box">
+                <h3 class="title">Pembahasan</h3>
+                <ul>
+                    <li><a href="#">Teknologi</a></li>
+                    <li><a href="#">Pendidikan</a></li>
+                </ul>
+            </div>
+            <div class="widget-box">
+                <h3 class="title">Welcome To my Website</h3>
+                <p>
+                  Website ini dibuat dengan tujuan untuk memenuhi tugas praktikum pemograman web.
+                </p>
+            </div>
+        </aside>
+    </section>
+    <footer>
+        <p>&copy; 2025 - Universitas Pelita Bangsa</p>
+    </footer>
+</div> <!-- Tutup container -->
+</body>
+</html>
